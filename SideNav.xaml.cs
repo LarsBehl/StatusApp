3392aa809b44ac8b4c4c9d9fd62b111a708761cf
Lars Behl <lars.behl@gmx.de>
2021-11-05 13:32:14 +0100

    [FEAT] STAT-06: added simple test sidenav

---
+++ SideNav.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace StatusApp
+{
+	public partial class SideNav : FlyoutPage
+	{
+		public SideNav()
+		{
+			InitializeComponent();
+		}
+	}
+}
